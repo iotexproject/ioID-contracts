79129749a7485195b04c09d5c405da55cdc4fb0b
ququzone <xueping.yang@gmail.com>
2024-04-11 16:34:32 +0800

    add create ERC6551 wallet

--- contracts/ioID.sol
+++ contracts/ioID.sol
@@ -3,19 +3,39 @@ pragma solidity ^0.8.19;
 
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import {IProject} from "./interfaces/IProject.sol";
+import {IERC6551Registry} from "./interfaces/IERC6551Registry.sol";
 
 contract ioID is ERC721Upgradeable {
     uint256 nextId;
     address public project;
+    address public registry;
+    address public implementation;
 
     // projectId => project minted count
     mapping(uint256 => uint256) public projectMinted;
     // ioID => projectId
     mapping(uint256 => uint256) public projectId;
 
-    function initialize(address _project, string calldata _name, string calldata _symbol) public initializer {
+    function initialize(
+        address _project,
+        address _registry,
+        address _implementation,
+        string calldata _name,
+        string calldata _symbol
+    ) public initializer {
         __ERC721_init(_name, _symbol);
         project = _project;
+        registry = _registry;
+        implementation = _implementation;
+    }
+
+    function createWallet(uint256 _id) external returns (address) {
+        _requireMinted(_id);
+        return IERC6551Registry(registry).createAccount(implementation, 0, block.chainid, address(this), _id);
+    }
+
+    function wallet(uint256 _id) external view returns (address) {
+        return IERC6551Registry(registry).account(implementation, 0, block.chainid, address(this), _id);
     }
 
     function mint(uint256 _projectId, address _owner) external returns (uint256) {
