c8476740a447ebb549c06969db2cfe3b4145aede
ququzone <xueping.yang@gmail.com>
2024-04-13 16:25:22 +0800

    add tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,6 @@ const config: HardhatUserConfig = {
     },
     dev: {
       url: 'http://127.0.0.1:8545',
-      accounts: accounts,
     },
     mainnet: {
       url: 'https://babel-api.mainnet.iotex.io/',
