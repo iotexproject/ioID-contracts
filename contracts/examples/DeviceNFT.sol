7288b7a7904c58bd63741a2d0b85d915e99fcee5
ququzone <xueping.yang@gmail.com>
2024-07-06 22:58:34 +0800

    add weight for device nft

--- contracts/examples/DeviceNFT.sol
+++ contracts/examples/DeviceNFT.sol
@@ -4,13 +4,18 @@ pragma solidity ^0.8.19;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract DeviceNFT is ERC721, Ownable {
+import "../interfaces/IDeviceNFT.sol";
+
+contract DeviceNFT is IDeviceNFT, ERC721, Ownable {
     event MinterConfigured(address indexed minter, uint256 minterAllowedAmount);
     event MinterRemoved(address indexed minter);
+
+    uint256 public immutable DEFAULT_WEIGHT = 1;
 
     mapping(address => bool) internal minters;
     mapping(address => uint256) internal minterAllowed;
     uint256 nextId;
+    mapping(uint256 => uint256) internal weights;
 
     constructor() ERC721("Example Device NFT", "EDN") {}
 
@@ -35,6 +40,14 @@ contract DeviceNFT is ERC721, Ownable {
     }
 
     function mint(address _to) external returns (uint256) {
+        return __mint(_to, 0);
+    }
+
+    function mint(address _to, uint256 _weight) external returns (uint256) {
+        return __mint(_to, _weight);
+    }
+
+    function __mint(address _to, uint256 _weight) internal returns (uint256) {
         require(_to != address(0), "zero address");
 
         uint256 mintingAllowedAmount = minterAllowed[msg.sender];
@@ -45,6 +58,17 @@ contract DeviceNFT is ERC721, Ownable {
 
         uint256 _tokenId = ++nextId;
         _mint(_to, _tokenId);
+        if (_weight != 0) {
+            weights[_tokenId] = _weight;
+        }
         return _tokenId;
+    }
+
+    function weight(uint256 tokenId) external view override returns (uint256) {
+        uint256 _weight = weights[tokenId];
+        if (_weight == 0) {
+            return DEFAULT_WEIGHT;
+        }
+        return _weight;
     }
 }
